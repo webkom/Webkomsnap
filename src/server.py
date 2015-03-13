83919eb0bf8d831bd295b8f86b335290f0146ddc
Andreas Broby <and.broby@gmail.com>
2015-03-13 03:00:04 +0100

    added a simple server for auth

--- src/server.py
+++ src/server.py
@@ -1,25 +1,40 @@
 import sqlite3
-from flask import Flask, request, jsonify, abort
+
+from flask import Flask, request, jsonify, abort, g
+from config import TOKEN, DATABASE
+
+app = Flask(__name__)
 
 
-class AuthServer:
-    app = Flask(__name__)
-
-    def __init__(self, token):
-        self.token = token
-
-    def run(self):
-        self.app.run()
-
-    @app.route('/create', methods=["POST", "GET"])
-    def auth_user():
-        if not request.json or 'username' not in request.json or 'token' not in request.json:
-            abort(400)
-       
-        token = request.json.get('token')
-        username = request.json.get('username')
-        if token == self.token:
+def run():
+    app.run()
 
 
-server = AuthServer("123")
-server.run()
+@app.before_request
+def open_db_connection():
+    g.db = sqlite3.connect(DATABASE)
+
+
+@app.teardown_appcontext
+def close_db_connection(exception):
+    if hasattr(g, 'db'):
+        g.db.close()
+
+
+@app.route('/create', methods=["POST"])
+def auth_user():
+    if not request.json or 'username' not in request.json or 'token' not in request.json:
+        return jsonify(error="Invalid json. Expected username and token fields"), 400
+
+    token = request.json.get('token')
+    username = request.json.get('username')
+
+    if token == TOKEN:
+        exists = g.db.execute("SELECT username FROM users WHERE username = ?", [username]).fetchone()
+        if exists is not None:
+            return jsonify(error="A user with that username is already registered"), 400
+        g.db.execute("INSERT INTO users ('username') VALUES (?)", [username])
+        g.db.commit()
+        return jsonify(username=username), 201
+    else:
+        abort(403)
