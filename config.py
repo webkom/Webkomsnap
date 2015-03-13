633fcc2011ad6cab69401a11951f46f0403c63d7
Andreas Broby <and.broby@gmail.com>
2015-03-13 06:40:41 +0100

    auth stuff

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 USERNAME = "webkom_story"
 PASSWORD = "78CuhGdu"
 DATABASE = os.path.join(os.path.abspath(os.curdir), "sqlite_db")
-TOKEN = "QCNct7Qb9JcNkqF"
+TOKEN = "QCNct7Qb9JcNkqF" # <- Top secret.
